a51e22474ab8de9f2003be1637ce6d392560c464
lucas reis <lucasreis-@outlook.com>
2025-04-10 18:01:46 -0300

    Adicionando peças no tabuleiro
    -Classe Rei e Torre

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -35,4 +35,9 @@ public class Tabuleiro {
 	public Peca peca(Posicao pos) {
 		return pecas[pos.getLinha()][pos.getColuna()];
 	}
+	
+	public void adicionarPeca(Peca peca, Posicao pos) {
+		pecas[pos.getLinha()][pos.getColuna()] = peca;
+		peca.posicao = pos;
+	}
 }
--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -1,6 +1,9 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaDeXadrez {
 	
@@ -8,6 +11,7 @@ public class PartidaDeXadrez {
 	
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		inicializarPartida();
 	}
 	
 	public PecaDeXadrez[][] getPecas() {
@@ -20,5 +24,13 @@ public class PartidaDeXadrez {
 		}
 
 		return pecas;
+	}
+	
+	private void inicializarPartida() {
+		tabuleiro.adicionarPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+		tabuleiro.adicionarPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
+		
+		tabuleiro.adicionarPeca(new Torre(tabuleiro, Cor.PRETO), new Posicao(0, 3));
+		tabuleiro.adicionarPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(7,3));
 	}
 }
